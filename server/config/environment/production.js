c8f18fba889acee7b2b2431c386e4a710cf180f1
nayanika <nayanika@quetzal.in>
2016-08-04 19:57:58 +0530

    sever setup

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -13,15 +13,22 @@ module.exports = {
           process.env.PORT ||
           8080,
 
-  sequelize: {
-    uri:  process.env.SEQUELIZE_URI ||
-          'sqlite://',
-    options: {
-      logging: false,
-      storage: 'dist.sqlite',
-      define: {
-        timestamps: false
-      }
-    }
-  }
+  qverify: {
+    username: process.env.QVERIFY_MYSQL_USER,
+    password: process.env.QVERIFY_MYSQL_PASS,
+    database: process.env.QVERIFY_MYSQL_DB,
+    host: process.env.QVERIFY_MYSQL_HOST,
+    dialect: 'mysql',
+    logging: console.log,
+    timezone: '+05:30',
+  },
+
+  MINIO: {
+    endPoint: process.env.MINIO_ENDPOINT,
+    accessKey: process.env.MINIO_ACCESS_KEY,
+    secretKey: process.env.MINIO_SECRET_KEY,
+    bucket: process.env.MINIO_BUCKET,
+    secure: false,
+    port: 8000,
+  },
 };
